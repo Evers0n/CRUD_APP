f4fa825b9645cf3f7d8a8b0c18b2c018db36c21e
Everson Faria <faria.everson2017@hotmail.com>
2024-08-01 15:30:26 -0300

    Edição_dos_dados_persistidos

--- CRUD_APP/Controllers/ProductsController.cs
+++ CRUD_APP/Controllers/ProductsController.cs
@@ -63,5 +63,78 @@ namespace CRUD_APP.Controllers
 
             return RedirectToAction("Index", "Products");
         }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _appDbContext.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            var productDto = new ProductDto()
+            {
+                Nome = product.Nome,
+                Marca = product.Marca,
+                Categoria = product.Categoria,
+                Preco = product.Preco,
+                Descricao = product.Descricao,
+            };
+
+            ViewData["Productid"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileNome;
+            ViewData["DataCriacao"] = product.DataCriacao.ToString("MM/dd/yyyy");
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = _appDbContext.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            if (!ModelState.IsValid)
+            {
+               
+
+                ViewData["Productid"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileNome;
+                ViewData["DataCriacao"] = product.DataCriacao.ToString("MM/dd/yyyy");
+
+                return View(productDto);
+            }
+
+            string newFileName = product.ImageFileNome;
+            if (productDto.ImageFile != null) 
+            {
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssFFF");
+                newFileName += Path.GetExtension(productDto.ImageFile.FileName);
+
+                string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);    
+                }
+
+                string oldImageFullPath = environment.WebRootPath + "/products/" + product.ImageFileNome;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            product.Nome = productDto.Nome;
+            product.Marca = productDto.Marca;
+            product.Categoria = productDto.Categoria;
+            product.Preco = productDto.Preco;
+            product.Descricao = productDto.Descricao;
+            product.ImageFileNome = newFileName;
+
+            _appDbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
+        }
     }
 }
